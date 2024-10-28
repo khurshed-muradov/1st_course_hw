261c7b1006f3807104316bc656e26dceb422fb6e
Khurshed Muradov <hurshed@gmail.com>
2024-10-28 15:22:33 +0500

    added task's solution

--- final_course_hw/Program.cs
+++ final_course_hw/Program.cs
@@ -1,6 +1,50 @@
-﻿// Задача: Написать программу, которая из имеющегося массива строк формирует новый массив из строк, длина которых меньше, либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+﻿// Задача: Написать программу, которая из имеющегося массива строк формирует новый массив из строк, 
+// длина которых меньше, либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры, 
+// либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, 
+// лучше обойтись исключительно массивами.
 
 // Примеры:
 // [“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
 // [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
 // [“Russia”, “Denmark”, “Kazan”] → []
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+
+        string[] inputArray = { "Work", "ing", "Tree", "Wtr" };
+
+
+        string[] resultArray = FilterArray(inputArray);
+
+
+        Console.WriteLine("Результат: [" + string.Join(", ", resultArray) + "]");
+    }
+
+    static string[] FilterArray(string[] array)
+    {
+
+        string[] tempArray = new string[array.Length];
+        int count = 0;
+
+
+        foreach (string str in array)
+        {
+
+            if (str.Length <= 3)
+            {
+                tempArray[count] = str;
+                count++;
+            }
+        }
+
+
+        string[] resultArray = new string[count];
+        Array.Copy(tempArray, resultArray, count);
+
+        return resultArray;
+    }
+}
